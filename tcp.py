3b5b74bfac10752b5fb397e47fedd897640a3dfc
basillone15 <basil.lone.25@dartmouth.edu>
2025-11-13 10:37:30 -0500

    adding func protos and editing send_packet imp specs

--- tcp.py
+++ tcp.py
@@ -1,5 +1,7 @@
 from scapy.all import *
 
+
+
 def sniff():
     sniff(iface="wlp0s20f3", filter="dst port [port num]", prn=parse_packet, store=False)
 
@@ -9,8 +11,28 @@ def parse_packet(packet):
         # data is bytes
         data = raw_layer.load
         
-        syn_bit: int = int.from_bytes(data
+        syn_bit: int = int.from_bytes(data)
         
         # extract the first four bytes
         sequence_number: int = int.from_bytes(data[0: 4], byteorder='big')
+    
+    return
+
+
+
+
+def build_packet(sequence_num, ack_num, flags, data, window):
+    return
+
+
+
+
+def send_packet(packet, ip, port):
+    return
+
+
+
+
+def handle_error(missing_packets):
+    return
 
