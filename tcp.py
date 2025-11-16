09ee47de08855cd5b078190b9cc1a07620b9a2e8
Tyler Schill <tylerjschill@gmail.com>
2025-11-15 20:51:14 -0500

    more work on parse packet

--- tcp.py
+++ tcp.py
@@ -8,34 +8,38 @@ def parse_packet(packet):
     raw_layer: bytes = packet.getlayer(Raw)
     if raw_layer:
         # data is bytes
-        rec_data: bytes = raw_layer.load        
-        syn_bit: int = int.from_bytes(rec_data[47], byteorder='big')
-        ack_bit: int = int.from_bytes(rec_data[44], byteorder='big')
+        rec_data: bytes = raw_layer.load
+        # rec_data is an array of bytes, not bits (so we index based on bytes
+        rec_flags: int = int.from_bytes(rec_data[10], byteorder='big')
+        #ack_bit: int = int.from_bytes(rec_data[44], byteorder='big')
 
-        if syn_bit == 1 and ack_bit == 0:
-            # extract the first four bytes
+        if rec_flags == 2: # 00000010
             rec_seq_num: int = int.from_bytes(rec_data[0:4], byteorder='big')
             seq_num: int = 1
             ack_num: int = rec_seq_num + 1
             flags: int = 18     # 00010010 (sets the ACK and SYN bits)
             data: bytes = b""
-            window: int = 0; # 0 for now until we figure out if we need it
+            window: int = 0 # 0 for now until we figure out if we need it
 
             build_packet(seq_num, ack_num, flags, data, window)
 
-        elif syn_bit == 1 and ack_bit == 1:
+        elif rec_flags == 18:
             rec_seq_num: int = int.from_bytes(rec_data[0:4], byteorder='big')
             seq_num: int = 0
             ack_num: int = rec_seq_num + 1
             flags: int = 16    # 00010000 (sets only ACK bit)
             data: bytes = b""
-            window: int - 0;
+            window: int = 0
 
             build_packet(seq_num, ack_num, flags, data, window)
 
-        else syn_bit == 0 and ack_bit == 1:
-            # do something
-            rec_ack: int = int.from_bytes(rec_data[], byteorder='big')
+        else rec_flags == 16:
+            rec_ack_num: int = int.from_bytes(rec_data[4:8], byteorder='big')
+            seq_num: int = 
+            ack_num: int =
+            flags: int = 
+            data: bytes = 
+            window: int = 0
     
     return
 
@@ -55,27 +59,4 @@ def send_packet(packet, ip, port):
 def handle_error(missing_packets):
     return
 
-        if syn_bit == 1 and ack_bit == 0:
-            # extract the first four bytes
-            rec_seq_num: int = int.from_bytes(rec_data[0:4], byteorder='big')
-            seq_num: int = 1
-            ack_num: int = rec_seq_num + 1
-            flags: int = 18     # 00010010 (sets the ACK and SYN bits)
-            data: bytes = b""
-            window: int = 0; # 0 for now until we figure out if we need it
 
-            build_packet(seq_num, ack_num, flags, data, window)
-
-        elif syn_bit == 1 and ack_bit == 1:
-            rec_seq_num: int = int.from_bytes(rec_data[0:4], byteorder='big')
-            seq_num: int = 0
-            ack_num: int = rec_seq_num + 1
-            flags: int = 16    # 00010000 (sets only ACK bit)
-            data: bytes = b""
-            window: int - 0;
-
-            build_packet(seq_num, ack_num, flags, data, window)
-
-        else syn_bit == 0 and ack_bit == 1:
-            # do something
-            rec_ack: int = int.from_bytes(rec_data[], byteorder='big')
