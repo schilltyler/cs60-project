6df88128de98ec8bb364c71c04ef959c8afe665e
basillone15 <basil.lone.25@dartmouth.edu>
2025-11-21 18:35:42 -0500

    adding some logic to main function to start connection and separating packet building

--- tcp.py
+++ tcp.py
@@ -1,6 +1,6 @@
 from scapy.all import *
 import threading
-from scapy.layers.inet import UDP
+from scapy.layers.inet import Ether, IP, UDP, Raw, sendp
 
 """
 FLAGS FORMAT:
@@ -12,6 +12,7 @@ SYN    = 00000010
 ACK    = 00010000
 SYNACK = 00010010
 """
+
 
 """
 Globals
@@ -28,11 +29,22 @@ packets_sent = []
 data_to_send = []
 
 
+"""
+Need to set a standard packet size to split data into multiple packets if needed
+"""
+PACKET_SIZE = 512
+
+
+
+
 def sniff():
     # using port 5555 for no reason in particular
     # was trying to find a port that is not dedicated to a service already
     # that way we do not get traffic other than our own on it
     sniff(iface="wlp0s20f3", filter="dst port 5555", prn=parse_packet, store=False)
+
+
+
 
 def set_data_packet_parameters(packet):
     src_port: int = 5555
@@ -47,7 +59,16 @@ def set_data_packet_parameters(packet):
     sequence_nums_sent.append(seq_num)
     data_sizes_sent.append(len(data))
 
+
+    # If splitting data into multiple packets is needed, do it here
+    # Convert data to bytes
+    # Split into chunks of PACKET_SIZE
+    # For each chunk, call build_packet with that chunk as data
+
     build_packet(src_port, dst_port, seq_num, ack_num, flags, data, window)
+
+
+
 
 """
 This function parses the TCP header within the data section
@@ -61,6 +82,7 @@ TODO:
 * add call to handle_error when sequence numbers are out of order
 """
 def parse_packet(packet):
+
     raw_layer: bytes = packet.getlayer(Raw)
     if raw_layer:
         # rec_data is an array of bytes, not bits (so we index based on bytes)
@@ -188,18 +210,19 @@ def build_packet(sport, dport, sequence_num, ack_num, flags, data, window):
         ack_num,        Integer: acknowledgment number
         sequence_num,   Integer: sequence number
 
-        flags,          String: 8-bit string representing TCP flags
+        flags,          Integer: Integer number representing TCP flags
 
         data, :
 
         window, :
     """
+    # UDP layer to indicate source and destination ports
+    udp_layer = UDP(sport=sport, dport=dport)
+
     # String to store the binary representation of the packet
     message = ""
 
     # Build the TCP header
-    message += format(sport, '016b')
-    message += format(dport, '016b')
     message += format(sequence_num, '032b')
     message += format(ack_num, '032b')
 
@@ -207,21 +230,22 @@ def build_packet(sport, dport, sequence_num, ack_num, flags, data, window):
     message += "0101"               # 4 bits        Offset (in 4byte words) to start of data section (Min:5 Max:15)
     message += "0000"               # 4 bits        These must be set to 0
 
-    message += flags
+    message += format(flags, '08b') # 8 bits        Flags
 
-    message += "\nwindow bits"      # 2 bytes
+    message += format(0, '016b')    # 2 bytes       Window Bits  - Need to figure this part out
 
-    message += "\nChecksum"         # 2 bytes
 
-    message += "\nurgent pointer"   # 2 bytes
-
+    # # Need logic to calculate checksum
+    # message += "\nChecksum"         # 2 bytes       Checksum - to be calculated later
+    # # Need logic to set ugrent pointer if needed
+    # message += "\nurgent pointer"   # 2 bytes       Urgent Pointer - only used if URG flag is set
     # Options TOREPLACE (if using this then need to change data offset and check how long this is)
 
     # Data TOREPLACE - NEED to see how packet size is determined and how to split data between packets
 
     return message
 
-mess = build_packet(1234, 5678, 0, 0, '00010000', "Hello, World!", 1024)
+mess = build_packet(1234, 5678, 0, 0, 16, "Hello, World!", 1024)
 print(mess)
 
 
@@ -256,11 +280,51 @@ Notes:
 * only want one side to send this though(?)
 """
 def main():
+    
+    # Need MAC and IP addresses from received packet to build response packets
+    # Also need a wat to initiate the connection from one side
+
+    mac = input("Enter destination MAC address: ")
+    ipad = input("Enter destination ip address: ")
+
+    # Need to have the option to either continuously listen for syn or send a syn
+    choice = input("Enter 'i' to initiate connection or 'l' to listen: ")
+
+    if choice == 'l':
+        # Start listening
+        pass
+
+    elif choice == 'i':
+        mac = input("Enter destination MAC address: ")
+        ipad = input("Enter destination ip address: ")
+
+        # Start listening
+        # Send syn
+
+    # Start listening in the background anyway on a separate thread
+    # On this thread handle user input to send data packets when needed
+
+    mac = "00:11:22:33:44:55"
+    ipad = "127.0.0.1"
+
+
+
+
     # populate the array of data
     for i in range(1, 50):
         data_to_send.append(f"data packet {i}")
 
     # send a SYN packet
+
+    # Call sniffing function in a separate thread
+    sniff_thread = threading.Thread(target=sniff)
+    sniff_thread.start()
+
+    
+    
+    # Send data packets here
+    # Can be 
+
     
     return
 
